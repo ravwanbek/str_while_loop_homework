2898f4d54f7a1f99b95a5ac7da514245f062eaa3
ravwanbek <90487558+ravwanbek@users.noreply.github.com>
2022-01-18 22:34:58 +0900

    10

--- While10.py
+++ While10.py
@@ -6,4 +6,12 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    idx=0
+    sum_num=0
+    while idx<len(s):
+        if int(s[idx])%2!=0:
+            sum_num+=int(s[idx])
+        idx+=1
+    
+    return sum_num
+print(main('11125'))
